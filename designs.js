a451c36a9be36b5baf652838e2f202ffda248646
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-11-15 17:29:15 +0100

    refactor: use loops to make and clear grid

--- designs.js
+++ designs.js
@@ -6,15 +6,30 @@ $(function() {
 	$('#pixel_canvas').on('click', 'td', function(e) {
 		$(e.target).css('background-color', $('#colorPicker').val());
 	});
-	// $('#pixel_canvas').on('click', 'td', function(e) {
- //        $(e.target).css('background-color', $('#colorPicker').val() );
- //    });
 });
 
 function makeGrid() {
 	const height = Number($('#input_height').val());
 	const width = Number($('#input_width').val());
-	const row = '<tr>' + '<td></td>'.repeat(width) + '</td>';
-	const rows = row.repeat(height);
-	$('#pixel_canvas').empty().append(rows);
+	// const row = '<tr>' + '<td></td>'.repeat(width) + '</td>';
+	// const rows = row.repeat(height);
+	// $('#pixel_canvas').empty().append(rows);
+	let rows = '';
+	for(let i = 0; i < height; i++) {
+		let row = '<tr>';
+		for(let j = 0; j < width; j++) {
+			row += '<td></td>';
+		}
+		row += '</tr>';
+		rows += row;
+	}
+	let oldRows = $('#pixel_canvas').children();
+	for(let i = 0; i < oldRows.length; i++) {
+		oldRows[i].remove();
+		console.log(i)
+		console.log($('#pixel_canvas').children())
+		console.log(oldRows)
+	}	
+	$('#pixel_canvas').append(rows);
+
 }
