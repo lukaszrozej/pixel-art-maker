212560b8cd66c40158e65c2fe80dfc7f5aafdfaa
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-11-18 14:51:51 +0100

    feat: add painting cell by dragging mouse

--- designs.js
+++ designs.js
@@ -9,11 +9,24 @@ $(function() {
 		makeGrid(width, height);
 		e.preventDefault();
 	});
+	// mouseup on body to handle a situation
+	// when user drags the mouse out of the table and then releases it
+	$('body').on('mouseup', stopPainting);
 });
 
 function startPainting(e) {
 	isPainting = true;
 	$(e.target).css('background-color', $('#colorPicker').val());
+}
+
+function paint(e) {
+	if (isPainting) {
+		$(e.target).css('background-color', $('#colorPicker').val());
+	}
+}
+
+function stopPainting() {
+	isPainting = false;
 }
 
 function makeGrid(width, height) {
@@ -23,6 +36,7 @@ function makeGrid(width, height) {
 		for(let j = 0; j < width; j++) {
 			const cell = $('<td></td>');
 			cell.on('mousedown', startPainting);
+			cell.on('mouseenter', paint);
 			row.append(cell);
 		}
 		canvas.append(row);
