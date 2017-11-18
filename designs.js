b1d1da1f69c2ad25a259b7d99a66be1e3dd0a367
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-11-18 17:19:55 +0100

    feat: add fill tool

--- designs.js
+++ designs.js
@@ -1,13 +1,21 @@
 const canvas = $('#pixel_canvas');
 
+let height = 0;
+let width = 0;
+
 let isPainting = false;
+let tool = 'brush';
 
 $(function() {
 	$("#sizePicker").submit(function(e) {
-		const height = Number($('#input_height').val());
-		const width = Number($('#input_width').val());
+		height = Number($('#input_height').val());
+		width = Number($('#input_width').val());
 		makeGrid(width, height);
 		e.preventDefault();
+	});
+	$('#tools').on('change', function() {
+	   tool = $('input[name=tool]:checked').val();
+	   console.log(tool);
 	});
 	// mouseup on body to handle a situation
 	// when user drags the mouse out of the table and then releases it
@@ -15,8 +23,13 @@ $(function() {
 });
 
 function startPainting(e) {
-	isPainting = true;
-	$(e.target).css('background-color', $('#colorPicker').val());
+	if (tool === 'brush') {
+		isPainting = true;
+		$(e.target).css('background-color', $('#colorPicker').val());
+		console.log('START')
+	}
+	// to prevent firing drag events
+	return false;
 }
 
 function paint(e) {
@@ -27,6 +40,52 @@ function paint(e) {
 
 function stopPainting() {
 	isPainting = false;
+	console.log('STOP')
+}
+
+function fill(e) {
+	if (tool != 'fill') {
+		return true;
+	}
+	const firstCell = $(e.target);
+	oldColor = firstCell.css('background-color');
+	newColor = $('#colorPicker').val();
+	if (newColor === oldColor) {
+		return true;
+	}
+	const cellsToFill = [firstCell];
+	while(cellsToFill.length > 0) {
+		const currentCell = cellsToFill.pop();
+		currentCell.css('background-color', newColor);
+		[x, y] = currentCell
+					.attr('id')
+					.split('-')
+					.map(Number);
+		if (x > 0) {
+			const leftCell = $('#' + (x-1) + '-' + y);
+			if (leftCell.css('background-color') === oldColor) {
+				cellsToFill.push(leftCell)
+			}
+		}
+		if (x < width-1) {
+			const rightCell = $('#' + (x+1) + '-' + y);
+			if (rightCell.css('background-color') === oldColor) {
+				cellsToFill.push(rightCell)
+			}
+		}
+		if (y > 0) {
+			const topCell = $('#' + x + '-' + (y-1));
+			if (topCell.css('background-color') === oldColor) {
+				cellsToFill.push(topCell)
+			}
+		}
+		if (x < height-1) {
+			const bottomCell = $('#' + x + '-' + (y+1));
+			if (bottomCell.css('background-color') === oldColor) {
+				cellsToFill.push(bottomCell)
+			}
+		}
+	}
 }
 
 function makeGrid(width, height) {
@@ -34,9 +93,10 @@ function makeGrid(width, height) {
 	for(let i = 0; i < height; i++) {
 		const row = $('<tr></tr>');
 		for(let j = 0; j < width; j++) {
-			const cell = $('<td></td>');
+			const cell = $('<td id="' + i + '-'  + j + '"></td>');
 			cell.on('mousedown', startPainting);
 			cell.on('mouseenter', paint);
+			cell.on('mouseup', fill);
 			row.append(cell);
 		}
 		canvas.append(row);
