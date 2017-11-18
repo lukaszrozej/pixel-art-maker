8404c40a848050b589a883730ca2afc8310273ff
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-11-18 14:44:42 +0100

    refactor: rename changeColor to startPainting

--- designs.js
+++ designs.js
@@ -1,5 +1,7 @@
 const canvas = $('#pixel_canvas');
 
+let isPainting = false;
+
 $(function() {
 	$("#sizePicker").submit(function(e) {
 		const height = Number($('#input_height').val());
@@ -9,9 +11,9 @@ $(function() {
 	});
 });
 
-function changeColor(e) {
+function startPainting(e) {
+	isPainting = true;
 	$(e.target).css('background-color', $('#colorPicker').val());
-	console.log('bla')
 }
 
 function makeGrid(width, height) {
@@ -20,7 +22,7 @@ function makeGrid(width, height) {
 		const row = $('<tr></tr>');
 		for(let j = 0; j < width; j++) {
 			const cell = $('<td></td>');
-			cell.on('mousedown', changeColor);
+			cell.on('mousedown', startPainting);
 			row.append(cell);
 		}
 		canvas.append(row);
