171e07855030048e73645f841fd31911756694c1
Łukasz Rożej <lukasz.rozej@gmail.com>
2017-11-28 14:55:35 +0100

    refactor: reorder design.js into sections

--- designs.js
+++ designs.js
@@ -27,56 +27,31 @@ $(function() {
 	let isPainting = false;
 	let tool = 'brush';
 
-	makeGrid(width,height);
+	// *************************************
+	// Helper functions
 
-	$('#new-grid').on('click', function() {
-		$('#canvas-container').slideUp('slow', function() {
-			$("#size-picker").slideDown('slow');
-		});
-	});
+	function hexToRGB(hex) {
+	    const r = parseInt(hex.slice(1, 3), 16);
+        const g = parseInt(hex.slice(3, 5), 16);
+        const b = parseInt(hex.slice(5, 7), 16);
+        return "rgb(" + r + ", " + g + ", " + b + ")";
+	}
 
-	$("#size-picker").submit(function(e) {
-		height = Number($('#input_height').val());
-		width = Number($('#input_width').val());
-		makeGrid(width, height);
-		$('#size-picker').slideUp('slow', function() {
-			$('#canvas-container').slideDown('slow');
-		});
-		e.preventDefault();
-	});
-	$('#colorPicker').on('change', function() {
-		newColor = hexToRGB($(this).val());
-	});
+	// *************************************
+	// History functions
 
-	$('input[name=tool]').on('change', function() {
-	   tool = $('input[name=tool]:checked').val();
-	});
-
-	$('#undo').on('click', function() {
-		momentInHistory--;
-		const action = history[momentInHistory];
-		for(item of action) {
-			item.cell.css('background-color', item.oldColor);
+	function recordCurrentAction() {
+		if (momentInHistory < history.length) {
+			history.splice(momentInHistory)
 		}
-		if (momentInHistory === 0) {
-			$('#undo').attr('disabled', 'disabled');
-		}
-		$('#redo').removeAttr('disabled');
-	});
-	$('#redo').on('click', function() {
-		const action = history[momentInHistory];
-		for(item of action) {
-			item.cell.css('background-color', item.newColor);
-		}
+		history.push(currentAction);
 		momentInHistory++;
-		if (momentInHistory === history.length) {
-			$('#redo').attr('disabled', 'disabled');
-		}
 		$('#undo').removeAttr('disabled');
-	});
-	// mouseup on body to handle a situation
-	// when user drags the mouse out of the table and then releases it
-	$('body').on('mouseup', stopPainting);
+		$('#redo').attr('disabled', 'disabled');
+	}
+
+	// *************************************
+	// Painting functions
 
 	function changeColor(cell, newColor) {
 		const oldColor = cell.css('background-color');
@@ -161,15 +136,8 @@ $(function() {
 		}
 	}
 
-	function recordCurrentAction() {
-		if (momentInHistory < history.length) {
-			history.splice(momentInHistory)
-		}
-		history.push(currentAction);
-		momentInHistory++;
-		$('#undo').removeAttr('disabled');
-		$('#redo').attr('disabled', 'disabled');
-	}
+	// *************************************
+	// make grid function
 
 	function makeGrid(width, height) {
 		momentInHistory = 0;
@@ -190,10 +158,58 @@ $(function() {
 		}
 	}
 
-	function hexToRGB(hex) {
-	    const r = parseInt(hex.slice(1, 3), 16);
-        const g = parseInt(hex.slice(3, 5), 16);
-        const b = parseInt(hex.slice(5, 7), 16);
-        return "rgb(" + r + ", " + g + ", " + b + ")";
-	}
+	// *************************************
+	// Event listeners
+
+	$('#new-grid').on('click', function() {
+		$('#canvas-container').slideUp('slow', function() {
+			$("#size-picker").slideDown('slow');
+		});
+	});
+
+	$("#size-picker").submit(function(e) {
+		height = Number($('#input_height').val());
+		width = Number($('#input_width').val());
+		makeGrid(width, height);
+		$('#size-picker').slideUp('slow', function() {
+			$('#canvas-container').slideDown('slow');
+		});
+		e.preventDefault();
+	});
+	$('#colorPicker').on('change', function() {
+		newColor = hexToRGB($(this).val());
+	});
+
+	$('input[name=tool]').on('change', function() {
+	   tool = $('input[name=tool]:checked').val();
+	});
+
+	$('#undo').on('click', function() {
+		momentInHistory--;
+		const action = history[momentInHistory];
+		for(item of action) {
+			item.cell.css('background-color', item.oldColor);
+		}
+		if (momentInHistory === 0) {
+			$('#undo').attr('disabled', 'disabled');
+		}
+		$('#redo').removeAttr('disabled');
+	});
+	$('#redo').on('click', function() {
+		const action = history[momentInHistory];
+		for(item of action) {
+			item.cell.css('background-color', item.newColor);
+		}
+		momentInHistory++;
+		if (momentInHistory === history.length) {
+			$('#redo').attr('disabled', 'disabled');
+		}
+		$('#undo').removeAttr('disabled');
+	});
+	// mouseup on body to handle a situation
+	// when user drags the mouse out of the table and then releases it
+	$('body').on('mouseup', stopPainting);
+
+
+	makeGrid(width,height);
 });
